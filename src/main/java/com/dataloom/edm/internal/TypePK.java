135a8d9c6fc073315386a0b872b9122ec9d612a6
Ho Chung Siu <hochung@kryptnostic.com>
2016-12-14 15:32:15 -0800

    More local testing passed. Bugfixed several Edm endpoints

--- src/main/java/com/dataloom/edm/internal/TypePK.java
+++ src/main/java/com/dataloom/edm/internal/TypePK.java
@@ -21,11 +21,11 @@ import com.google.common.base.Preconditions;
 public class TypePK {
     @PartitionKey(
         value = 0 )
-    @NotBlank
+    @NotBlank( message = "Namespace cannot be empty." )
     protected String                 namespace;
     @ClusteringColumn(
         value = 0 )
-    @NotBlank
+    @NotBlank( message = "Name cannot be empty." )
     protected String                 name;
 
     @Column(
@@ -40,7 +40,6 @@ public class TypePK {
     }
 
     public TypePK setNamespace( String namespace ) {
-        Preconditions.checkArgument( StringUtils.isNotBlank( namespace ), "Namespace cannot be null" );
         this.namespace = namespace;
         return this;
     }
@@ -50,7 +49,6 @@ public class TypePK {
     }
 
     public TypePK setName( String name ) {
-        Preconditions.checkArgument( StringUtils.isNotBlank( name ), "Name cannot be null" );
         this.name = name;
         return this;
     }
--- src/main/java/com/dataloom/edm/validation/GetSchemasRequestValidator.java
+++ src/main/java/com/dataloom/edm/validation/GetSchemasRequestValidator.java
@@ -3,6 +3,8 @@ package com.dataloom.edm.validation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.dataloom.edm.requests.GetSchemasRequest;
 
 public class GetSchemasRequestValidator implements ConstraintValidator<ValidateGetSchemasRequest, GetSchemasRequest> {
@@ -16,6 +18,10 @@ public class GetSchemasRequestValidator implements ConstraintValidator<ValidateG
             return true;
         }
 
-        return ( req.getName().isPresent() && req.getNamespace().isPresent() ) || !req.getName().isPresent();
+        if ( req.getName().isPresent() && StringUtils.isNotBlank( req.getName().get() ) ) {
+            return req.getNamespace().isPresent() && StringUtils.isNotBlank( req.getNamespace().get() );
+        } else {
+            return true;
+        }
     }
 }
