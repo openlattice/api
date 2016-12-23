edda9358e2d764141642576a9c470cb6545015f8
Ho Chung Siu <hochung@kryptnostic.com>
2016-12-22 16:35:33 -0800

    finalizing newPermissionsApi

--- src/main/java/com/dataloom/authorization/NewPermissionsApi.java
+++ src/main/java/com/dataloom/authorization/NewPermissionsApi.java
@@ -1,67 +1,28 @@
 package com.dataloom.authorization;
 
-import java.util.Map;
+import java.util.List;
 
-import com.dataloom.authorization.requests.AclResponse;
-import com.dataloom.authorization.requests.SecurableObjectRequest;
-
-import retrofit2.http.GET;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 
 public interface NewPermissionsApi {
-    String PERMISSIONS       = "permissions";
-    String ALL_PATH          = "all";
-    String DETAILS_BASE_PATH = "details";
+    String PERMISSIONS = "permissions";
 
     /**
-     * Used to load a list of all entity sets that caller can managed via the Permissions API.
+     * Add, removes, or sets the ace for a particular acl key. Successful only if user is the owner of acl key.
      * 
-     * @return A map of all administerable entity sets.
+     * @param req The acl key, the principals, and the aces to set for that particular ace key.
+     * @return The aces for the acl key, after applying the request changes.
      */
-    @GET( PERMISSIONS )
-    Map<String, AclKeyInfo> getAdministerableEntitySets();
+    @PATCH( PERMISSIONS )
+    Acl updateAcl( AclData req );
 
     /**
-     * Retrieves the acl of specified user for a particular acl key. If no user is specified, acl of current user is
-     * returned. If user is specified, acl is only returned if current user is owner of the acl key.
+     * Retrieves the acl for a particular acl key. Only return if user is the owner of acl key.
      * 
-     * @param req The securable object and the principal for which to return an acl. Any aces included in the request
-     *            will be ignored.
+     * @param aclKeys The acl key.
      * @return The aces for the requested acl key.
      */
     @POST( PERMISSIONS )
-    AclResponse getAcl( SecurableObjectRequest req );
-
-    /**
-     * Retrieves the acl with full details (whenever applicable) for a particular acl key. For example, if acl key
-     * corresponds to an entity set, acls for all property types of the entity set would be returned.
-     * 
-     * @param req The securable object and the principal, where an acl with full details would be returned. Any aces
-     *            included in the request will be ignored.
-     * @return The aces for the requested acl key.
-     */
-    @POST( PERMISSIONS + "/" + DETAILS_BASE_PATH )
-    AclResponse getDetailedAcl( SecurableObjectRequest req );
-
-    /**
-     * Add, removes, or sets the acl for a particular acl key.
-     * 
-     * @param req The acl key, the principal, and the aces to set for that particular ace key.
-     * @return The acl for acl key, after applying the request changes.
-     */
-    @PATCH( PERMISSIONS )
-    Iterable<AclResponse> updateAcl( SecurableObjectRequest req );
-
-    /**
-     * Retrieves all acls for a particular acl key.
-     * 
-     * Only return if user is the owner of acl key.
-     * 
-     * @param req The securable object for which to return an acl. Any principal and aces included in the request
-     *            will be ignored.
-     * @return The aces for the requested acl key.
-     */
-    @POST( PERMISSIONS + "/" + ALL_PATH )
-    Iterable<AclResponse> getAllAcl( SecurableObjectRequest req );
+    Acl getAcl( List<AclKey> aclKeys );
 }
