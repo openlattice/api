eb89f170bf59c9349d0cdc898a9e4b9b692169c6
Ho Chung Siu <hochung@kryptnostic.com>
2017-01-09 11:28:13 -0800

    bugfix: rename NewDataController/NewDataApi to DataController/DataApi; remove CreateEntityRequestSerializerTest

--- src/main/java/com/dataloom/data/DataApi.java
+++ src/main/java/com/dataloom/data/DataApi.java
@@ -13,7 +13,7 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
-public interface NewDataApi {
+public interface DataApi {
     /**
      * To discuss paths later; perhaps batch this with EdmApi paths
      */
