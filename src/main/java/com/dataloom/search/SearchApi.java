7c9cf391653b90d926f15430b96601b959d2dd4b
Katherine <katherine@kryptnostic.com>
2017-01-14 12:00:41 -0800

    explain behavior of endpoint

--- src/main/java/com/dataloom/search/SearchApi.java
+++ src/main/java/com/dataloom/search/SearchApi.java
@@ -16,12 +16,26 @@ public interface SearchApi {
 	String ENTITY_TYPE_ID = "eid";
 	String PROPERTY_TYPE_ID = "pid";
 		
+	/*
+	 * The query, entityType, and propertyTypes params are all optional,
+	 * but at least one must be specified otherwise an error will be thrown.
+	 * All specified params are required to be present in each entity set returned.
+	 * If entityType and propertyTypes are both specified, the propertyTypes param
+	 * will be ignored.
+	 */
 	@GET( SEARCH )
 	String executeQueryJson(
 			@Query(KEYWORD) String query,
 			@Query(ENTITY_TYPE_ID) UUID entityType,
 			@Query(PROPERTY_TYPE_ID) Set<UUID> propertyTypes );
 	
+	/*
+	 * The query, entityType, and propertyTypes params are all optional,
+	 * but at least one must be specified otherwise an error will be thrown.
+	 * All specified params are required to be present in each entity set returned.
+	 * If entityType and propertyTypes are both specified, the propertyTypes param
+	 * will be ignored.
+	 */
 	@POST( SEARCH )
 	Iterable<Map<String, Object>> executeQuery(
 			@Query(KEYWORD) String query,
