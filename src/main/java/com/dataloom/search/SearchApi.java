9b0aa907226b996770e14e4ec7707bb4f9d34f2a
Katherine <katherine@kryptnostic.com>
2017-01-16 11:34:04 -0800

    get instead of post, all params in SearchRequest obj

--- src/main/java/com/dataloom/search/SearchApi.java
+++ src/main/java/com/dataloom/search/SearchApi.java
@@ -1,12 +1,11 @@
 package com.dataloom.search;
 
 import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
+
+import com.dataloom.search.requests.SearchRequest;
 
 import retrofit2.http.Body;
-import retrofit2.http.POST;
-import retrofit2.http.Query;
+import retrofit2.http.GET;
 
 public interface SearchApi {
 
@@ -22,17 +21,16 @@ public interface SearchApi {
      * All specified params are required to be present in each entity set returned.
      * If entityType and propertyTypes are both specified, the propertyTypes param
      * will be ignored.
-     *
-     * @param query         An optional parameter the specifies they keywords used to perform the search.
-     * @param entityType    An optional parameter specifying the entity type of the entity sets that will be returned.
-     * @param propertyTypes An optional parameter specifying the property types of the entity sets that will be returned.
+     * 
+     * @param request A JSON object that contains up to three parameters: "query" (specifies the keywords used to perform
+     * the search), "eid" (UUID of the entity type of the entity sets that will be returned), and "pid" (a set of UUIDs
+     * of property types belonging to the entity sets that will be returned). All three of these parameters are optional,
+     * but at least one must be specified otherwise an error will be thrown. If eid and pid are both specified, the pid
+     * param will be ignored.
      * @return JSON string of matching entity set metadata
      */
-    @POST( SEARCH )
-    String executeQueryJson(
-            @Query( KEYWORD ) String query,
-            @Query( ENTITY_TYPE_ID ) UUID entityType,
-            @Body Set<UUID> propertyTypes );
+    @GET( SEARCH )
+    String executeQueryJson( @Body SearchRequest request );
 
     /**
      * The query, entityType, and propertyTypes params are all optional,
@@ -40,16 +38,15 @@ public interface SearchApi {
      * All specified params are required to be present in each entity set returned.
      * If entityType and propertyTypes are both specified, the propertyTypes param
      * will be ignored.
-     *
-     * @param query         An optional parameter the specifies they keywords used to perform the search.
-     * @param entityType    An optional parameter specifying the entity type of the entity sets that will be returned.
-     * @param propertyTypes An optional parameter specifying the property types of the entity sets that will be returned.
-     * @return Iterable of {@code Map<String, Object>} where each map corresponds to matching entity set metadata
+     * 
+     * @param request A JSON object that contains up to three parameters: "query" (specifies the keywords used to perform
+     * the search), "eid" (UUID of the entity type of the entity sets that will be returned), and "pid" (a set of UUIDs
+     * of property types belonging to the entity sets that will be returned). All three of these parameters are optional,
+     * but at least one must be specified otherwise an error will be thrown. If eid and pid are both specified, the pid
+     * param will be ignored.
+     * @return JSON string of matching entity set metadata
      */
-    @POST( SEARCH_JAVA )
-    Iterable<Map<String, Object>> executeQuery(
-            @Query( KEYWORD ) String query,
-            @Query( ENTITY_TYPE_ID ) UUID entityType,
-            @Body Set<UUID> propertyTypes );
+    @GET( SEARCH_JAVA )
+    Iterable<Map<String, Object>> executeQuery( @Body SearchRequest request );
 
 }
