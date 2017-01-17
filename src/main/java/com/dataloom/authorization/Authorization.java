86b59ff31f97dc61e7ad46996849874d32fefd4f
Ho Chung Siu <hochung@kryptnostic.com>
2017-01-16 17:35:19 -0800

    minor fixes of authorizations

--- src/main/java/com/dataloom/authorization/Authorization.java
+++ src/main/java/com/dataloom/authorization/Authorization.java
@@ -10,27 +10,27 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Authorization {
     @JsonProperty( SerializationConstants.ACL_OBJECT_PATH )
     private List<UUID>               aclKey;
-    @JsonProperty( SerializationConstants.PERMISSIONS_MAP )
-    private Map<Permission, Boolean> permissionsMap;
+    @JsonProperty( SerializationConstants.PERMISSIONS )
+    private Map<Permission, Boolean> permissions;
 
     public Authorization( List<UUID> aclKey, Map<Permission, Boolean> permissionsMap ) {
         this.aclKey = aclKey;
-        this.permissionsMap = permissionsMap;
+        this.permissions = permissionsMap;
     }
 
     public List<UUID> getAclKey() {
         return aclKey;
     }
 
-    public Map<Permission, Boolean> getPermissionsMap() {
-        return permissionsMap;
+    public Map<Permission, Boolean> getPermissions() {
+        return permissions;
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ( ( permissionsMap == null ) ? 0 : permissionsMap.hashCode() );
+        result = prime * result + ( ( permissions == null ) ? 0 : permissions.hashCode() );
         result = prime * result + ( ( aclKey == null ) ? 0 : aclKey.hashCode() );
         return result;
     }
@@ -41,9 +41,9 @@ public class Authorization {
         if ( obj == null ) return false;
         if ( getClass() != obj.getClass() ) return false;
         Authorization other = (Authorization) obj;
-        if ( permissionsMap == null ) {
-            if ( other.permissionsMap != null ) return false;
-        } else if ( !permissionsMap.equals( other.permissionsMap ) ) return false;
+        if ( permissions == null ) {
+            if ( other.permissions != null ) return false;
+        } else if ( !permissions.equals( other.permissions ) ) return false;
         if ( aclKey == null ) {
             if ( other.aclKey != null ) return false;
         } else if ( !aclKey.equals( other.aclKey ) ) return false;
@@ -52,7 +52,7 @@ public class Authorization {
 
     @Override
     public String toString() {
-        return "Authorization [aclKey=" + aclKey + ", permissionsMap=" + permissionsMap + "]";
+        return "Authorization [aclKey=" + aclKey + ", permissionsMap=" + permissions + "]";
     }
 
 }
