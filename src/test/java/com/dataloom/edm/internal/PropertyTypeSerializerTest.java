5c8418add161f42ed0e09aa8a80f0960898993c4
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-01-19 20:37:32 -0800

    Fix property type test

--- src/test/java/com/dataloom/edm/internal/PropertyTypeSerializerTest.java
+++ src/test/java/com/dataloom/edm/internal/PropertyTypeSerializerTest.java
@@ -7,8 +7,7 @@ import com.dataloom.data.serializers.FullQualifedNameJacksonSerializer;
 import com.dataloom.mapstores.TestDataFactory;
 import com.dataloom.serializer.BaseJacksonSerializationTest;
 
-public class PropertyTypeSerializerTest extends BaseJacksonSerializationTest<EntitySet> {
-
+public class PropertyTypeSerializerTest extends BaseJacksonSerializationTest<PropertyType> {
     @BeforeClass
     public static void configureSerializer() {
         FullQualifedNameJacksonSerializer.registerWithMapper( mapper );
@@ -18,12 +17,12 @@ public class PropertyTypeSerializerTest extends BaseJacksonSerializationTest<Ent
     }
 
     @Override
-    protected EntitySet getSampleData() {
-        return TestDataFactory.entitySet();
+    protected PropertyType getSampleData() {
+        return TestDataFactory.propertyType();
     }
 
     @Override
-    protected Class<EntitySet> getClazz() {
-        return EntitySet.class;
+    protected Class<PropertyType> getClazz() {
+        return PropertyType.class;
     }
 }
