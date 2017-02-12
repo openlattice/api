0b2b1263de7b267e99cb525cd5ce61ae3f96458a
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-02-12 14:49:56 -0800

    Implement equals for EntityKey

--- src/main/java/com/dataloom/data/EntityKey.java
+++ src/main/java/com/dataloom/data/EntityKey.java
@@ -6,13 +6,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.UUID;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * Uniquely identifies a version of an entity
+ *
  * @author Matthew Tamayo-Rios &lt;matthew@kryptnostic.com&gt;
  */
-public class EntityKey implements Comparable<EntityKey>{
-    private final UUID syncId;
-    private final UUID entitySetId;
+public class EntityKey implements Comparable<EntityKey> {
+    private final UUID   syncId;
+    private final UUID   entitySetId;
     private final String entityId;
 
     @JsonCreator
@@ -20,9 +23,9 @@ public class EntityKey implements Comparable<EntityKey>{
             @JsonProperty( SerializationConstants.SYNC_ID ) UUID syncId,
             @JsonProperty( SerializationConstants.ENTITY_SET_ID ) UUID entitySetId,
             @JsonProperty( SerializationConstants.ENTITY_ID ) String entityId ) {
-        this.syncId = syncId;
-        this.entitySetId = entitySetId;
-        this.entityId = entityId;
+        this.syncId = checkNotNull( syncId );
+        this.entitySetId = checkNotNull( entitySetId );
+        this.entityId = checkNotNull( entityId );
     }
 
     @JsonProperty( SerializationConstants.SYNC_ID )
@@ -40,14 +43,36 @@ public class EntityKey implements Comparable<EntityKey>{
         return entityId;
     }
 
-    @Override public int compareTo( EntityKey o ) {
-        int result =  syncId.compareTo( o.syncId );
+    @Override public boolean equals( Object o ) {
+        if ( this == o )
+            return true;
+        if ( !( o instanceof EntityKey ) )
+            return false;
 
-        if( result == 0 ) {
+        EntityKey entityKey = (EntityKey) o;
+
+        if ( !syncId.equals( entityKey.syncId ) )
+            return false;
+        if ( !entitySetId.equals( entityKey.entitySetId ) )
+            return false;
+        return entityId.equals( entityKey.entityId );
+    }
+
+    @Override public int hashCode() {
+        int result = syncId.hashCode();
+        result = 31 * result + entitySetId.hashCode();
+        result = 31 * result + entityId.hashCode();
+        return result;
+    }
+
+    @Override public int compareTo( EntityKey o ) {
+        int result = syncId.compareTo( o.syncId );
+
+        if ( result == 0 ) {
             result = entitySetId.compareTo( o.entitySetId );
         }
 
-        if( result == 0 ) {
+        if ( result == 0 ) {
             result = entityId.compareTo( o.entityId );
         }
 
