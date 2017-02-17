c0606ec9f5f73f83c5f0a222078096f9d4393504
Katherine <katherine@kryptnostic.com>
2017-02-16 19:32:17 -0800

    eclipse can auto generate these things :o

--- src/main/java/com/dataloom/search/requests/SearchResult.java
+++ src/main/java/com/dataloom/search/requests/SearchResult.java
@@ -4,47 +4,42 @@ import java.util.List;
 import java.util.Map;
 
 public class SearchResult {
-    
-    private final long numHits;
+
+    private final long                      numHits;
     private final List<Map<String, Object>> hits;
-    
+
     public SearchResult( long numHits, List<Map<String, Object>> hits ) {
         this.numHits = numHits;
         this.hits = hits;
     }
-    
+
     public long getNumHits() {
         return numHits;
     }
-    
+
     public List<Map<String, Object>> getHits() {
         return hits;
     }
-    
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + Long.valueOf( numHits ).hashCode();
         result = prime * result + ( ( hits == null ) ? 0 : hits.hashCode() );
+        result = prime * result + (int) ( numHits ^ ( numHits >>> 32 ) );
         return result;
     }
-    
+
     @Override
     public boolean equals( Object obj ) {
-        if ( this == obj ) {
-            return true;
-        }
-        if ( !( obj instanceof SearchResult ) ) {
-            return false;
-        }
+        if ( this == obj ) return true;
+        if ( obj == null ) return false;
+        if ( getClass() != obj.getClass() ) return false;
         SearchResult other = (SearchResult) obj;
-        if ( numHits != other.numHits ) {
-            return false;
-        }
-        if ( !hits.equals( other.hits ) ) {
-            return false;
-        }
+        if ( hits == null ) {
+            if ( other.hits != null ) return false;
+        } else if ( !hits.equals( other.hits ) ) return false;
+        if ( numHits != other.numHits ) return false;
         return true;
     }
 }
