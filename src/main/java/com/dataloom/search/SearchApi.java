c30457a31abe0822fb272562e426e66c2c66d6c1
Katherine <katherine@kryptnostic.com>
2017-03-12 15:21:17 -0700

    move top utilizers into AnalysisApi

--- src/main/java/com/dataloom/search/SearchApi.java
+++ src/main/java/com/dataloom/search/SearchApi.java
@@ -1,7 +1,5 @@
 package com.dataloom.search;
 
-import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 import com.dataloom.edm.EntitySet;
@@ -9,7 +7,6 @@ import com.dataloom.search.requests.AdvancedSearch;
 import com.dataloom.search.requests.Search;
 import com.dataloom.search.requests.SearchResult;
 import com.dataloom.search.requests.SearchTerm;
-import com.google.common.collect.SetMultimap;
 
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -98,21 +95,4 @@ public interface SearchApi {
      */
     @POST( BASE + ORGANIZATIONS )
     SearchResult executeOrganizationSearch( @Body SearchTerm searchTerm );
-
-    /**
-     * Returns the top rows in the entity set, ordered by the sum of the number of items in each of the property types
-     * provided
-     * 
-     * @param entitySetId The id of the entity set to sort and return results for
-     * @param numResults The number of results to return
-     * @param propertyTypeIds A set of property types which may have multiple values in an entity set. The results will
-     *            be ordered by the total number of values across all property types provided (i.e. the sum of all the
-     *            property types' value array size).
-     * @return
-     */
-    @POST( BASE + ANALYSIS + ENTITY_SET_ID_PATH + NUM_RESULTS_PATH )
-    List<SetMultimap<UUID, Object>> getTopUtilizers(
-            @Path( ENTITY_SET_ID ) UUID entitySetId,
-            @Path( NUM_RESULTS ) int numResults,
-            @Body Set<UUID> propertyTypeIds );
 }
