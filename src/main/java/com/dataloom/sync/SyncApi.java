2f1fcdf0575dcc0482b6cb832f5ed8f26d43da63
Katherine <katherine@kryptnostic.com>
2017-04-04 17:22:58 -0700

    endpoint for choosing latest sync id

--- src/main/java/com/dataloom/sync/SyncApi.java
+++ src/main/java/com/dataloom/sync/SyncApi.java
@@ -3,6 +3,7 @@ package com.dataloom.sync;
 import java.util.UUID;
 
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface SyncApi {
@@ -12,12 +13,14 @@ public interface SyncApi {
     String BASE               = SERVICE + CONTROLLER;
 
     String ENTITY_SET_ID      = "entitySetId";
+    String SYNC_ID            = "syncId";
 
-    String ENTITY_SET_ID_PATH = "{" + ENTITY_SET_ID + "}";
+    String ENTITY_SET_ID_PATH = "/{" + ENTITY_SET_ID + "}";
+    String SYNC_ID_PATH       = "/{" + SYNC_ID + "}";
     String NEW                = "/new";
 
     /**
-     * Generates a new sync id for an entity set.
+     * Generates a new sync id for an entity set. If the entity set has no latest sync id, the new one is set as latest.
      * 
      * @param entitySetId The id of the entity set to generate a new sync id for.
      * @return A new time-uuid generated in data source api.
@@ -27,13 +30,22 @@ public interface SyncApi {
             @Path( ENTITY_SET_ID ) UUID entitySetId );
 
     /**
-     * Retrieves the latest sync id for the given entity set, and creates one if one doesn't exist yet
+     * Retrieves the latest sync id for the given entity set
      * 
      * @param entitySetId The id of the entity set to load the latest sync id for.
      * @return The latest time-uuid for the entity set.
      */
     @GET( BASE + "/" + ENTITY_SET_ID_PATH )
-    UUID getLatestSyncId(
-            @Path( ENTITY_SET_ID ) UUID entitySetId );
+    UUID getLatestSyncId( @Path( ENTITY_SET_ID ) UUID entitySetId );
+
+    /**
+     * Sets the latest syncId for the entity set to the specified syncId. By default, reads for the entity set will come
+     * from the latest syncId, and writes will be written to the latest sync, unless otherwise specified.
+     * 
+     * @param entitySetId The id of the entity set to set the latest sync id for.
+     * @param syncId the sync id that will be set as latest
+     */
+    @POST( BASE + ENTITY_SET_ID_PATH + SYNC_ID_PATH )
+    Void setLatestSyncId( @Path( ENTITY_SET_ID ) UUID entitySetId, @Path( SYNC_ID ) UUID syncId );
 
 }
