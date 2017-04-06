3b86081b17bcb079e1904c3b15f6ef8f0df48ab0
Katherine <katherine@kryptnostic.com>
2017-04-06 15:27:03 -0700

    tickets are their own field, don't need to be associated with entities/conenctions

--- src/main/java/com/dataloom/data/requests/BulkDataCreation.java
+++ src/main/java/com/dataloom/data/requests/BulkDataCreation.java
@@ -1,6 +1,6 @@
 package com.dataloom.data.requests;
 
-import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 import com.dataloom.client.serialization.SerializationConstants;
@@ -8,24 +8,32 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class BulkDataCreation {
-    private Map<UUID, Entity>     entities;
-    private Map<UUID, Connection> connections;
+    private Set<UUID>       tickets;
+    private Set<Entity>     entities;
+    private Set<Connection> connections;
 
     @JsonCreator
     public BulkDataCreation(
-            @JsonProperty( SerializationConstants.ENTITIES ) Map<UUID, Entity> entities,
-            @JsonProperty( SerializationConstants.CONNECTIONS ) Map<UUID, Connection> connections ) {
+            @JsonProperty( SerializationConstants.SYNC_TICKETS ) Set<UUID> tickets,
+            @JsonProperty( SerializationConstants.ENTITIES ) Set<Entity> entities,
+            @JsonProperty( SerializationConstants.CONNECTIONS ) Set<Connection> connections ) {
+        this.tickets = tickets;
         this.entities = entities;
         this.connections = connections;
     }
 
+    @JsonProperty( SerializationConstants.SYNC_TICKETS )
+    public Set<UUID> getTickets() {
+        return tickets;
+    }
+
     @JsonProperty( SerializationConstants.ENTITIES )
-    public Map<UUID, Entity> getEntities() {
+    public Set<Entity> getEntities() {
         return entities;
     }
 
     @JsonProperty( SerializationConstants.CONNECTIONS )
-    public Map<UUID, Connection> getConnections() {
+    public Set<Connection> getConnections() {
         return connections;
     }
 
@@ -33,8 +41,9 @@ public class BulkDataCreation {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ( ( entities == null ) ? 0 : entities.hashCode() );
         result = prime * result + ( ( connections == null ) ? 0 : connections.hashCode() );
+        result = prime * result + ( ( entities == null ) ? 0 : entities.hashCode() );
+        result = prime * result + ( ( tickets == null ) ? 0 : tickets.hashCode() );
         return result;
     }
 
@@ -44,12 +53,15 @@ public class BulkDataCreation {
         if ( obj == null ) return false;
         if ( getClass() != obj.getClass() ) return false;
         BulkDataCreation other = (BulkDataCreation) obj;
-        if ( entities == null ) {
-            if ( other.entities != null ) return false;
-        } else if ( !entities.equals( other.entities ) ) return false;
         if ( connections == null ) {
             if ( other.connections != null ) return false;
         } else if ( !connections.equals( other.connections ) ) return false;
+        if ( entities == null ) {
+            if ( other.entities != null ) return false;
+        } else if ( !entities.equals( other.entities ) ) return false;
+        if ( tickets == null ) {
+            if ( other.tickets != null ) return false;
+        } else if ( !tickets.equals( other.tickets ) ) return false;
         return true;
     }
 
