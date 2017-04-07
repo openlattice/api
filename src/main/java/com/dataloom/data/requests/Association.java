1bd042e7dab72ec513a541ded892a4d020b5d3ca
Katherine <katherine@kryptnostic.com>
2017-04-07 15:17:37 -0700

    hash code/equals/to string

--- src/main/java/com/dataloom/data/requests/Association.java
+++ src/main/java/com/dataloom/data/requests/Association.java
@@ -46,4 +46,41 @@ public class Association {
     public SetMultimap<UUID, Object> getDetails() {
         return details;
     }
+    
+    @Override
+    public String toString() {
+        return "Association [key=" + key + ", src=" + src + ", dst=" + dst + ", details=" + details + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ( ( details == null ) ? 0 : details.hashCode() );
+        result = prime * result + ( ( dst == null ) ? 0 : dst.hashCode() );
+        result = prime * result + ( ( key == null ) ? 0 : key.hashCode() );
+        result = prime * result + ( ( src == null ) ? 0 : src.hashCode() );
+        return result;
+    }
+
+    @Override
+    public boolean equals( Object obj ) {
+        if ( this == obj ) return true;
+        if ( obj == null ) return false;
+        if ( getClass() != obj.getClass() ) return false;
+        Association other = (Association) obj;
+        if ( details == null ) {
+            if ( other.details != null ) return false;
+        } else if ( !details.equals( other.details ) ) return false;
+        if ( dst == null ) {
+            if ( other.dst != null ) return false;
+        } else if ( !dst.equals( other.dst ) ) return false;
+        if ( key == null ) {
+            if ( other.key != null ) return false;
+        } else if ( !key.equals( other.key ) ) return false;
+        if ( src == null ) {
+            if ( other.src != null ) return false;
+        } else if ( !src.equals( other.src ) ) return false;
+        return true;
+    }
 }
