0cd9e92cf337617d62ad0f24b8c3540000e37181
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-04-20 18:20:08 -0600

    Remove EntitySetData from Loom API

--- src/main/java/com/dataloom/data/DataApi.java
+++ src/main/java/com/dataloom/data/DataApi.java
@@ -65,7 +65,7 @@ public interface DataApi {
             @Body Map<String, SetMultimap<UUID, Object>> entities );
 
     @GET( BASE + "/" + ENTITY_DATA + "/" + SET_ID_PATH )
-    EntitySetData loadEntitySetData(
+    Iterable<SetMultimap<FullQualifiedName, Object>> loadEntitySetData(
             @Path( SET_ID ) UUID entitySetId,
             @Query( FILE_TYPE ) FileType fileType,
             @Query( TOKEN ) String token );
