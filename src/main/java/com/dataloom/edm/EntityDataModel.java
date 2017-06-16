47c975a5760de874f3ec5cc9f3823d14c99fae8c
Matthew Tamayo-Rios <matthew@openlattice.com>
2017-06-16 04:07:08 -0700

    Update urls and constants

--- src/main/java/com/dataloom/edm/EntityDataModel.java
+++ src/main/java/com/dataloom/edm/EntityDataModel.java
@@ -2,6 +2,7 @@ package com.dataloom.edm;
 
 import com.dataloom.edm.type.EntityType;
 import com.dataloom.edm.type.PropertyType;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class EntityDataModel {
@@ -11,6 +12,7 @@ public class EntityDataModel {
     private final Iterable<PropertyType> propertyTypes;
     private final Iterable<EntitySet>    entitySets;
 
+    @JsonCreator
     public EntityDataModel(
             @JsonProperty( EdmApi.NAMESPACES ) Iterable<String> namespaces,
             @JsonProperty( EdmApi.SCHEMAS ) Iterable<Schema> schemas,
@@ -49,4 +51,44 @@ public class EntityDataModel {
         return entitySets;
     }
 
+    //TODO: Null check at creation to avoid doing it on every equals
+    @Override
+    public boolean equals( Object o ) {
+        if ( this == o )
+            return true;
+        if ( !( o instanceof EntityDataModel ) )
+            return false;
+
+        EntityDataModel that = (EntityDataModel) o;
+
+        if ( namespaces != null ? !namespaces.equals( that.namespaces ) : that.namespaces != null )
+            return false;
+        if ( schemas != null ? !schemas.equals( that.schemas ) : that.schemas != null )
+            return false;
+        if ( entityTypes != null ? !entityTypes.equals( that.entityTypes ) : that.entityTypes != null )
+            return false;
+        if ( propertyTypes != null ? !propertyTypes.equals( that.propertyTypes ) : that.propertyTypes != null )
+            return false;
+        return entitySets != null ? entitySets.equals( that.entitySets ) : that.entitySets == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = namespaces != null ? namespaces.hashCode() : 0;
+        result = 31 * result + ( schemas != null ? schemas.hashCode() : 0 );
+        result = 31 * result + ( entityTypes != null ? entityTypes.hashCode() : 0 );
+        result = 31 * result + ( propertyTypes != null ? propertyTypes.hashCode() : 0 );
+        result = 31 * result + ( entitySets != null ? entitySets.hashCode() : 0 );
+        return result;
+    }
+
+    @Override public String toString() {
+        return "EntityDataModel{" +
+                "namespaces=" + namespaces +
+                ", schemas=" + schemas +
+                ", entityTypes=" + entityTypes +
+                ", propertyTypes=" + propertyTypes +
+                ", entitySets=" + entitySets +
+                '}';
+    }
 }
