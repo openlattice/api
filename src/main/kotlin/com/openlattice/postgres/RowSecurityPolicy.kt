2383141ae3f11c444603cac16a771c8ddd01b72a
Piper Keyes <piper@openlattice.com>
2020-03-05 17:18:44 -0800

    improve row policy class

--- src/main/kotlin/com/openlattice/postgres/RowSecurityPolicy.kt
+++ src/main/kotlin/com/openlattice/postgres/RowSecurityPolicy.kt
@@ -1,18 +1,14 @@
 package com.openlattice.postgres
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.openlattice.authorization.Permission
 import com.openlattice.client.serialization.SerializationConstants
-import java.util.*
 
 //TODO link to pg_policies
 
 data class RowSecurityPolicy(
-        @JsonProperty(SerializationConstants.NAME) val policyName: String,
-        @JsonProperty(SerializationConstants.ORGANIZATION_ID) val orgId: UUID,
-        @JsonProperty(SerializationConstants.TABLE_ID) val tableId: UUID,
-        @JsonProperty(SerializationConstants.PERMISSIONS) val permissions: Set<Permission>,
-        @JsonProperty(SerializationConstants.ROLES) val users: Set<String>,
-        @JsonProperty(SerializationConstants.FILTER) val readFilter: String,
-        @JsonProperty(SerializationConstants.CHECK) val writeCheck: String
+        @JsonProperty(SerializationConstants.PERMISSIVENESS) val permissiveness: String,
+        @JsonProperty(SerializationConstants.USER_ID) val userIds: List<String>,
+        @JsonProperty(SerializationConstants.PERMISSIONS) val privilege: PostgresPrivileges,
+        @JsonProperty(SerializationConstants.READ_FILTER) val readFilter: String,
+        @JsonProperty(SerializationConstants.WRITE_FILTER) val writeFilter: String
 )
