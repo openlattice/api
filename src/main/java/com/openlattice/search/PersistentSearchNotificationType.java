f71c208fe1109096fb7a26e42295e3e688af90c3
Solomon Tang <solomoncztang@gmail.com>
2020-06-01 12:11:21 -0700

    Add new notification type enums

--- src/main/java/com/openlattice/search/PersistentSearchNotificationType.java
+++ src/main/java/com/openlattice/search/PersistentSearchNotificationType.java
@@ -3,5 +3,7 @@ package com.openlattice.search;
 public enum PersistentSearchNotificationType {
     ALPR_ALERT,
     BHR_ALERT,
-    CODEX_ALERT
+    CODEX_ALERT,
+    CRISIS_AFFILIATION_ALERT,
+    CRISIS_VETERAN_ALERT,
 }
