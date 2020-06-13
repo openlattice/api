a57ee979aa6c8891d4328e6095455c58c8595f1e
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-06-12 18:12:02 -0700

    Add postgres storage configuration class.

---
+++ src/main/kotlin/com/openlattice/data/storage/PostgresStorageConfiguration.kt
@@ -0,0 +1,9 @@
+package com.openlattice.data.storage
+
+/**
+ *
+ * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
+ */
+class PostgresStorageConfiguration : StorageConfiguration {
+    
+}
