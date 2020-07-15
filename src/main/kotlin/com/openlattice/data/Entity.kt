48b7310ffcbefaa4b1b2711f063a0f5ef7bbebfb
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-07-15 12:49:03 -0700

    WIP

--- src/main/kotlin/com/openlattice/data/Entity.kt
+++ src/main/kotlin/com/openlattice/data/Entity.kt
@@ -6,7 +6,7 @@ import java.time.OffsetDateTime
 import java.util.*
 
 /**
- *
+ * This class is used as the data representation for both lin
  * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
  */
 data class Entity(
@@ -16,3 +16,11 @@ data class Entity(
     val version: Long = getValue(VERSION_ID.id).first() as Long
     val lastWrite: OffsetDateTime = OffsetDateTime.parse(getValue(LAST_WRITE_ID.id).first() as String )
 }
+
+/**
+ * So in order to represent both linked entities and regular entities we need to identify what entity sets
+ * each particular value came from
+ *
+ * { property-type-id: { value: {} , entity_set_id
+ */
+
